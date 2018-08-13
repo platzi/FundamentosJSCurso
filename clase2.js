f00f0217256b780d53e5775e0066338af8d32380
Sacha Lifszyc <sacha.lifszyc@gmail.com>
2018-08-13 13:46:01 -0700

    Clase 2: Strings

---
+++ clase2.js
@@ -0,0 +1,11 @@
+var nombre = 'Sacha', apellido = 'Lifszyc'
+
+var nombreEnMayusculas = nombre.toUpperCase()
+var apellidoEnMinusculas = apellido.toLowerCase()
+
+var primeraLetraDelNombre = nombre.charAt(0)
+var cantidadDeLetrasDelNombre = nombre.length
+
+var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`
+
+var str = nombre.substr(1, 2)
