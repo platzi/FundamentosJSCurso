fcb3176ceeb77032dd853ea434ecbf87d550067c
Sacha Lifszyc <sacha.lifszyc@gmail.com>
2018-08-13 16:37:27 -0700

    Clase 10 - Condicionales

---
+++ clase10.js
@@ -0,0 +1,44 @@
+var sacha = {
+  nombre: 'Sacha',
+  apellido: 'Lifszyc',
+  edad: 28,
+  ingeniero: false,
+  cocinero: false,
+  cantante: true,
+  dj: false,
+  guitarrista: false,
+  drone: false
+}
+
+function imprimirProfesiones(persona) {
+  console.log(`${persona.nombre} es:`)
+
+  if (persona.ingeniero) {
+    console.log('Ingeniero')
+  } else {
+    console.log('No es ingeniero')
+  }
+
+  if (persona.cocinero) {
+    console.log('Cocinero')
+  }
+
+  if (persona.dj) {
+    console.log('DJ')
+  }
+
+  if (persona.cantante) {
+    console.log('Cantante')
+  }
+
+  if (persona.guitarrista) {
+    console.log('Gutiarrista')
+  }
+
+  if (persona.drone) {
+    console.log('Piloto de drone')
+  }
+
+}
+
+imprimirProfesiones(sacha)
