3ecf1b8ce1601128efbdc2f915dbaf454ede82e2
Sacha Lifszyc <sacha.lifszyc@gmail.com>
2018-08-13 14:54:05 -0700

    Clase 4 - Funciones

---
+++ clase4.js
@@ -0,0 +1,12 @@
+var nombre = 'Sacha', edad = 28
+
+function imprimirEdad(n, e) {
+  console.log(`${n} tiene ${e} años`)
+}
+
+imprimirEdad(nombre, edad)
+imprimirEdad('Vicky', 28)
+imprimirEdad('Eric', 24)
+imprimirEdad('Darío', 27)
+imprimirEdad(25, 'Carlos')
+imprimirEdad('Juan')
