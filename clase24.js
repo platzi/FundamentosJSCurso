f57789794a712b14b3f8257363677cc8ba764538
Sacha Lifszyc <sacha.lifszyc@gmail.com>
2018-08-14 13:14:43 -0700

    Clase 24 - La verdad oculta sobre las clases en JavaScript

---
+++ clase24.js
@@ -0,0 +1,36 @@
+function heredaDe(prototipoHijo, prototipoPadre) {
+  var fn = function () {}
+  fn.prototype = prototipoPadre.prototype
+  prototipoHijo.prototype = new fn
+  prototipoHijo.prototype.constructor = prototipoHijo
+}
+
+function Persona(nombre, apellido, altura) {
+  this.nombre = nombre
+  this.apellido = apellido
+  this.altura = altura
+}
+
+Persona.prototype.saludar = function () {
+  console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
+}
+
+Persona.prototype.soyAlto = function () {
+  return this.altura > 1.8
+}
+
+function Desarrollador(nombre, apellido) {
+  this.nombre = nombre
+  this.apellido = apellido
+}
+
+heredaDe(Desarrollador, Persona)
+
+Desarrollador.prototype.saludar = function () {
+  console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y soy desarrollador/a`)
+}
+
+
+// var sacha = new Persona('Sacha', 'Lifszyc', 1.72)
+// var erika = new Persona('Erika', 'Luna', 1.65)
+// var arturo = new Persona('Arturo', 'Martinez', 1.89)
