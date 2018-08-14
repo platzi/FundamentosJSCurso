680beee37f0965646ef55bc50cdecb0552b26cb4
Sacha Lifszyc <sacha.lifszyc@gmail.com>
2018-08-14 10:18:54 -0700

    Clase 22 - This en las arrow functions

---
+++ clase23.js
@@ -0,0 +1,17 @@
+function Persona(nombre, apellido, altura) {
+  this.nombre = nombre
+  this.apellido = apellido
+  this.altura = altura
+}
+
+Persona.prototype.saludar = function () {
+  console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
+}
+
+Persona.prototype.soyAlto = function () {
+  return this.altura > 1.8
+}
+
+var sacha = new Persona('Sacha', 'Lifszyc', 1.72)
+var erika = new Persona('Erika', 'Luna', 1.65)
+var arturo = new Persona('Arturo', 'Martinez', 1.89)
