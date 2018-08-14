1a84350dc17bd4ae480a2af7cae9a3801fa1c385
Sacha Lifszyc <sacha.lifszyc@gmail.com>
2018-08-14 14:25:16 -0700

    Clase 29 - Haciendo multiples requests

---
+++ clase29.js
@@ -0,0 +1,16 @@
+const API_URL = 'https://swapi.co/api/'
+const PEOPLE_URL = 'people/:id'
+const opts = { crossDomain: true }
+
+const onPeopleResponse = function (persona) {
+  console.log(`Hola, yo soy ${persona.name}`)
+}
+
+function obtenerPersonaje(id) {
+  const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+  $.get(url, opts, onPeopleResponse)
+}
+
+obtenerPersonaje(1)
+obtenerPersonaje(2)
+obtenerPersonaje(3)
