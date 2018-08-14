bc4fc9171a7917ecd98ae3067d4278570a11b16c
Sacha Lifszyc <sacha.lifszyc@gmail.com>
2018-08-14 08:54:05 -0700

    Clase 17 - Intro a Arrrays

---
+++ clase17.js
@@ -0,0 +1,42 @@
+var sacha = {
+  nombre: 'Sacha',
+  apellido: 'Lifszyc',
+  altura: 1.72
+}
+
+var alan = {
+  nombre: 'Alan',
+  apellido: 'Perez',
+  altura: 1.86
+}
+
+var martin = {
+  nombre: 'Martin',
+  apellido: 'Gomez',
+  altura: 1.85
+}
+
+var dario = {
+  nombre: 'Dario',
+  apellido: 'Juarez',
+  altura: 1.71
+}
+
+var vicky = {
+  nombre: 'Vicky',
+  apellido: 'Zapata',
+  altura: 1.56
+}
+
+var paula = {
+  nombre: 'Paula',
+  apellido: 'Barros',
+  altura: 1.76
+}
+
+var personas = [sacha, alan, martin, dario, vicky, paula]
+
+for (var i = 0; i < personas.length; i++) {
+  var persona = personas[i]
+  console.log(`${persona.nombre} mide ${persona.altura}mts`)
+}
