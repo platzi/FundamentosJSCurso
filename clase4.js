287a4699e1c71a5916c485ef1fc3adaaae25f968
kira100 <franciscocastillosc@gmail.com>
2019-02-18 09:28:11 -0400

    Funciones y pasar parametros

---
+++ clase4.js
@@ -0,0 +1,12 @@
+var nombre = 'Francisco', edad = 19
+
+function imprimirEdad(n, e){
+	console.log(`${n} tiene ${e} años`)
+
+}
+imprimirEdad(nombre, edad)
+
+imprimirEdad('Jiulli', 25)
+
+imprimirEdad('Javi', 15)
+
