922447e72c373fe7adc0a31a4007197db458f554
kira100 <franciscocastillosc@gmail.com>
2019-02-20 10:56:03 -0400

    Transformar arrays sin cambiar el original

---
+++ clase19.js
@@ -0,0 +1,47 @@
+var sacha = {
+    nombre: 'Sacha',
+    apellido: 'Lifszyc',
+    altura: 1.72
+}
+
+var alan = {
+    nombre: 'Alan',
+    apellido: 'Perez',
+    altura: 1.86
+}
+
+var martin = {
+    nombre: 'Martin',
+    apellido: 'Gomez',
+    altura: 1.85
+}
+
+var dario = {
+    nombre: 'Dario',
+    apellido: 'Juarez',
+    altura: 1.71
+}
+
+var vicky = {
+    nombre: 'Vicky',
+    apellido: 'Zapata',
+    altura: 1.56
+}
+
+var paula = {
+    nombre: 'Paula',
+    apellido: 'Barros',
+    altura: 1.76
+}
+
+var personas = [sacha, alan, martin, dario, vicky, paula]
+
+const pasarAlturaACms = persona =>({
+             ...persona,
+        altura: persona.altura * 100 //Cambia a centimetros
+
+})
+
+var personasCms = personas.map(pasarAlturaACms) //Modifica el array original
+
+console.log(personasCms)
