fbcb440d5689a8596709114e22d139d1e52d0d40
deybit <deybitgutierrez@gmail.com>
2019-02-21 09:48:58 -0400

    Local and global variables

--- clase5.js
+++ clase5.js
@@ -1,4 +1,14 @@
-var nombre = 'Sacha'
+var nombre = 'Fabiana'
+
+function imprimirNombreEnMayusculas(nombre){
+  nombre = nombre.toUpperCase()
+  console.log('Nombre dentro de la funcion: '+nombre)
+}
+
+imprimirNombreEnMayusculas(nombre)
+console.log('Nombre fuera de la funcion: ' + nombre)
+
+/*var nombre = 'Sacha'
 
 function imprimirNombreEnMayusculas(nombre) {
   nombre = nombre.toUpperCase()
@@ -6,3 +16,4 @@ function imprimirNombreEnMayusculas(nombre) {
 }
 
 imprimirNombreEnMayusculas(nombre)
+*/
